1137e08c7ce19e41b793380596b43917ba5d4624
william nascimento <williamnnmiranda@gmail.com>
2023-05-08 13:44:49 -0300

    feat: :sparkles: added global style

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import router from './routes.tsx'
 import { RouterProvider } from 'react-router-dom'
+import GlobalStyle from './components/globalStyle/index.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyle />
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
